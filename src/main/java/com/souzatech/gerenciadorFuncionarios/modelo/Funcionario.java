157768a08e41a4faa11d73f37df60d00837042e8
Fábio Souza <fhssouza@gmail.com>
2023-05-13 10:25:08 -0300

    Entidade Funcionario

---
+++ src/main/java/com/souzatech/gerenciadorFuncionarios/modelo/Funcionario.java
@@ -0,0 +1,34 @@
+package com.souzatech.gerenciadorFuncionarios.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Funcionario {
+
+    @Id //javax.persistence.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+    private String email;
+    private String cargo;
+    private BigDecimal salario;
+
+    @Enumerated(EnumType.STRING)
+    private FuncionarioSetor setor;
+
+    public void setSetor(FuncionarioSetor setor) {
+        this.setor = setor;
+    }
+
+}
---
+++ src/main/java/com/souzatech/gerenciadorFuncionarios/modelo/FuncionarioSetor.java
@@ -0,0 +1,18 @@
+package com.souzatech.gerenciadorFuncionarios.modelo;
+
+public enum FuncionarioSetor {
+
+    TECNOLOGIA("tecnologia"),
+    RH("rh"),
+    DIRETORIA("diretoria");
+
+    private String value;
+
+    private FuncionarioSetor(String value) {
+        this.value = value;
+    }
+
+    public String getSetor() {
+        return value;
+    }
+}
